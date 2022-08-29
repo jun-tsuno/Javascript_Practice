486f5d2b88815410314541215f6ca2dd355f7c73
jun-tsuno <jun55tsuno@gmail.com>
2022-08-28 22:57:53 -0700

    first commit

---
+++ picture-in-picture/script.js
@@ -0,0 +1,18 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass video element, then play.
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error) {
+        console.log('whoops, error here', error);
+    }
+}
+
+// On Load
+selectMediaStream();
